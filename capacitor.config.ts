3826ee0c0fc5c96ded9b3003cfe5e0ffec4e46b3
benherrmann625-maker <benherrmann625@gmail.com>
2026-04-26 10:39:45 +0200

    calendar.v6.1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: "app.yourvoice.organizer",
   appName: "Your Voice",
   webDir: "mobile-shell",
-  bundledWebRuntime: false,
   server: {
     androidScheme: "https",
   },
